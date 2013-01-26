db3b851a276e2de4957aabafa20bd3bb25ab18af
hgsk <hagihararyosuke@gmail.com>
2013-01-26 12:13:58 +0900

    プロジェクト作成

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sophi::Application.config.secret_token = 'a85b24af8a2ce76927cac97fb6429cb9b80615709069dd304e24a90937f5da00a6624061f6910fe3e974cfa8f2abfd54aa1ff54b2f6ef4d6de7c0b934d23c820'
