e42c89f4842e16eb6f7fa8e7887b246e5a27fb4d
hgsk <hagihararyosuke@gmail.com>
2013-02-14 21:49:11 +0900

    add statement_ref attr result turn to bson

--- app/models/statement.rb
+++ app/models/statement.rb
@@ -7,8 +7,10 @@ class Statement
   embeds_one :activity, store_as: :object
   embeds_one :agent, store_as: :object
   embeds_one :statement, store_as: :object
+  embeds_one :statement_ref, store_as: :object
 
-  field :result, type: String
+  embeds_one :result
+
   field :context, type: String
   field :timestamp, type: String
   field :stored, type: String
